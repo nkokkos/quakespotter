20ce59b6e0007383ee0f645f725154cce5d8c52d
Nick Kokkos <nkokkos@gmail.com>
2009-12-18 14:10:51 +0200

    Fixed errors in view files, added routes and more..

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
   
-#  map.resources :quakes_past_hour, :controller => 'quakes_past_hour', :action => 'index'
-#  map.resources :quakes_past_day, :controller => 'quakes_past_day', :action => 'index'
+  map.resource :quakes_past_hour,  :controller => 'quakes_past_hour', :action => 'show'
+  map.resource :quakes_past_day, :controller => 'quakes_past_day', :action => 'show'
 
-  map.resource :quakes_past_hour,  :controller => 'quakes_past_hour', :action => 'index'
-  map.resource :quakes_past_day, :controller => 'quakes_past_day', :action => 'index'
-
+  map.root :controller => "quakes_past_hour" 
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
