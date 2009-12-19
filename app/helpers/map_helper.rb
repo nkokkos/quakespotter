e758aac9b2ef6db9f9e1a8a42321897799c5e9b7
Nick Kokkos <nkokkos@gmail.com>
2009-12-19 19:02:25 +0200

    modified views a little bit..

--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -3,13 +3,14 @@ module MapHelper
   def init_map
     run_map_script do
       map = Google::Map.new(:controls => [:small_map, :map_type],
-                            :center => {:latitude => -33.947, :longitude => 18.462},
-                            :zoom => 12)
+                        #    :center => {:latitude => -33.947, :longitude => 18.462},
+                            :zoom => 1)
 
-      map.click do |script, location|
-        map.open_info_window(:location => location, :html => 'hello cape town!')
-      end
-    end
+ #     map.click do |script, location|
+ #       map.open_info_window(:location => location, :html => 'hello cape town!')
+ #     end
+ 
+   end
   end
   
 end
