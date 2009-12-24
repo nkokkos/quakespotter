4f5718e328a2e7bae3759559f05aff6dc0cea542
Nick Kokkos <nkokkos@gmail.com>
2009-12-24 14:23:53 +0200

    added fastercsv plugin, played a little bit with parsing csv files and graphing with eschaton plugin

--- app/controllers/quakes_past_day_controller.rb
+++ app/controllers/quakes_past_day_controller.rb
@@ -3,8 +3,13 @@ class QuakesPastDayController < ApplicationController
   def index
   end
 
-  def show
-  end
-
-
+   def show
+    @a= []
+    
+    FasterCSV.each("#{RAILS_ROOT}/tmp/eqs1hour-M1.txt", :headers => true, :col_sep => ',') do |row|
+      @a << [ row[5], row[6] ]
+    end
+    
+    end
+      
 end
--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -1,16 +1,33 @@
+require 'fastercsv'
+
 module MapHelper
   
   def init_map
+    @a= []
+    
+    FasterCSV.foreach("#{RAILS_ROOT}/tmp/eqs1hour-M1.txt", :headers => true, :col_sep => ',') do |row|
+      @a << [ row[5], row[6] ]
+    end
+  
     run_map_script do
       map = Google::Map.new(:controls => [:small_map, :map_type],
                         #    :center => {:latitude => -33.947, :longitude => 18.462},
-                            :zoom => 1)
+                            :zoom => 2)
 
  #     map.click do |script, location|
  #       map.open_info_window(:location => location, :html => 'hello cape town!')
  #     end
  
-   end
+ #    @a.each { |x| map.add_marker :location => x }
+     
+     hops = [ [38.7847,-122.7702], [34.0206,-117.2071] ]
+     
+      hops.each do |location|
+       map.add_marker :location =>location
+     end
+
+    end
+  
   end
   
 end
