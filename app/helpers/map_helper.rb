c91eb2bdd4f45729badcce0635ae4f9a5af9e8a5
Nick Kokkos <nkokkos@gmail.com>
2009-12-28 10:41:20 +0200

    fixed issue with parsing csv files on windows (used the "rb" attribute while saving the file in /tmp

--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -11,8 +11,8 @@ module MapHelper
     # For 1 day quakes: http://earthquake.usgs.gov/earthquakes/catalogs/eqs1day-M1.txt
     # For 1 hour quakes: http://earthquake.usgs.gov/earthquakes/catalogs/eqs1hour-M1.txt
 
-    FasterCSV.foreach("#{RAILS_ROOT}/tmp/eqs1day-M1.txt", :headers => true, :col_sep => ',') do |row|
-      @points << [ row[4], row[5] ] # column 4 and 5 are defined as log and lat of the quake point.
+    FasterCSV.foreach("#{RAILS_ROOT}/tmp/eqs1day-M1.txt", :headers => true, :col_sep => ',', :row_sep => "\r\n") do |row|
+      @points << [ row[4], row[5] ] # column 4 and 5 are defined as Lat,Lon of the quake point.
     end
   
     run_map_script do # run_map_script is a method defined inside eschaton google map plugin
@@ -31,13 +31,13 @@ module MapHelper
         map.add_marker :location =>location
       end
 
-    end #run_init_script  
+    end #run_map_script
 
   end   #def init_map_eqs1day
 
 
  def init_map_eqs1hour
-    
+  
     @points =  []  # define an array to hold the points to be pinned on the map
     
     # Use fastercsv library to read data from the csv file and extract the coordinates of each
@@ -45,8 +45,8 @@ module MapHelper
     # For 1 day quakes: http://earthquake.usgs.gov/earthquakes/catalogs/eqs1day-M1.txt
     # For 1 hour quakes: http://earthquake.usgs.gov/earthquakes/catalogs/eqs1hour-M1.txt
 
-    FasterCSV.foreach("#{RAILS_ROOT}/tmp/eqs1hour-M1.txt", :headers => true, :col_sep => ',') do |row|
-      @points << [ row[4], row[5] ] # column 4 and 5 are defined as log and lat of the quake point.
+    FasterCSV.foreach("#{RAILS_ROOT}/tmp/eqs1hour-M1.txt", :headers => true, :col_sep => ',', :row_sep => "\r\n") do |row|
+       @points << [ row[4], row[5] ] # column 4 and 5 are defined as Lat,Lon of the quake point.
     end
   
     run_map_script do # run_map_script is a method defined inside eschaton google map plugin
@@ -62,11 +62,12 @@ module MapHelper
     # @points.each { |x| map.add_marker :location => x } # a different way to add marker
       
      @points.each do |location|
-        map.add_marker :location =>location
+        map.add_marker :location => location
       end
 
-    end #run_init_script  
+    end #run_map_script  
 
   end   #def init_map_eqs1hour
+
 
 end # MapHelper module
--- lib/tasks/quake_updates.rake
+++ lib/tasks/quake_updates.rake
@@ -6,11 +6,12 @@ desc "Updates quakes for hour and day periods. Graps the files from http://earth
 task :update do
   # Grab file for 1 hour quakes 
   req = Net::HTTP.get(URI.parse('http://earthquake.usgs.gov/earthquakes/catalogs/eqs1hour-M1.txt'))
-  File.open("#{RAILS_ROOT}/tmp/eqs1hour-M1.txt",'w') { |f| f.write(req) }
+  # http://stackoverflow.com/questions/287713/how-do-i-remove-carriage-returns-with-ruby (write file in binary mode for reasons explained in link)
+  File.open("#{RAILS_ROOT}/tmp/eqs1hour-M1.txt",'wb') { |f| f.write(req) }
 
   # Grab file for 1 day quakes 
   req = Net::HTTP.get(URI.parse('http://earthquake.usgs.gov/earthquakes/catalogs/eqs1day-M1.txt'))
-  File.open("#{RAILS_ROOT}/tmp/eqs1day-M1.txt",'w') { |f| f.write(req) }
+  File.open("#{RAILS_ROOT}/tmp/eqs1day-M1.txt",'wb') { |f| f.write(req) }
 
 end
 
